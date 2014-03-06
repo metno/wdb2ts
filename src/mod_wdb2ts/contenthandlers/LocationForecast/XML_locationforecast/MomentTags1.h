8e5ce9e409809b5d501b50267753ab40d04625ee
Børge Moe <borge.moe@met.no>
2014-03-06 15:31:35 +0100

    New moment data output-generator that collects data to be used in the SymbolGeneration.

---
+++ src/mod_wdb2ts/contenthandlers/LocationForecast/XML_locationforecast/MomentTags1.h
@@ -0,0 +1,95 @@
+/*
+    wdb - weather and water data storage
+
+    Copyright (C) 2007 met.no
+
+    Contact information:
+    Norwegian Meteorological Institute
+    Box 43 Blindern
+    0313 OSLO
+    NORWAY
+    E-mail: wdb@met.no
+  
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+    MA  02110-1301, USA
+*/
+
+#ifndef __WDB2TS_MOMENTTAGS1_H__
+#define __WDB2TS_MOMENTTAGS1_H__
+
+#include <float.h>
+#include <math.h>
+#include <IXmlTemplate.h>
+#include <PointDataHelper.h>
+#include <LocationData.h>
+#include <SymbolContext.h>
+#include <ProjectionHelper.h>
+#include <WeatherSymbolDataBuffer.h>
+
+namespace {
+wdb2ts::SymbolDataElement dummy;
+}
+
+namespace wdb2ts {
+
+
+class MomentTags1 : public IXmlTemplate
+{
+	LocationElem *pd;
+	SymbolDataElement *symData;
+	const ProjectionHelper *projectionHelper;
+	
+public:
+	MomentTags1():
+		pd(0), symData( &dummy ), projectionHelper( 0 ){
+	}
+	
+	MomentTags1( LocationElem &pointData,
+			    const ProjectionHelper *projectionHelper_ )
+		:  symData( &dummy ), projectionHelper( projectionHelper_ )
+		{
+			init( pointData );
+		}
+
+	MomentTags1( LocationElem &pointData,
+				SymbolDataElement &symData_,
+				const ProjectionHelper *projectionHelper_ )
+			:  symData( &symData_ ), projectionHelper( projectionHelper_ )
+			{
+				init( pointData );
+			}
+
+
+	MomentTags1( LocationElem &pointData, SymbolDataElement &symData_ )
+			: symData( &symData_ ), projectionHelper( 0 )
+	{
+		init( pointData );
+	}
+
+	
+	virtual ~MomentTags1(){
+	}
+	
+	void init( LocationElem &pointData );
+	
+	float getTemperatureProability( float temp, bool tryHard )const;
+		
+	virtual void output( std::ostream &out, const std::string &indent );
+};
+
+}
+
+
+#endif 
