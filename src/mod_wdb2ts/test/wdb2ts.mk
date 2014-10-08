98fc7136cc2148ad00cec2035c1a1434c553b76a
Børge Moe <borge.moe@met.no>
2014-10-08 13:30:58 +0200

    Add new test.

--- src/mod_wdb2ts/test/wdb2ts.mk
+++ src/mod_wdb2ts/test/wdb2ts.mk
@@ -13,7 +13,9 @@ wdb2TsUnitTest_SOURCES += 	src/mod_wdb2ts/test/UrlParamTest.cpp \
                      src/mod_wdb2ts/test/ProjectionTest.h \
                      src/mod_wdb2ts/test/ProjectionTest.cpp \
                      src/mod_wdb2ts/test/SymbolTest.h \
-                     src/mod_wdb2ts/test/SymbolTest.cpp       
+                     src/mod_wdb2ts/test/SymbolTest.cpp \
+                     src/mod_wdb2ts/test/VariosTests.h \
+                     src/mod_wdb2ts/test/VariosTests.cpp      
 
 EXTRA_DIST +=				src/mod_wdb2ts/test/wdb2ts.mk \
 							src/mod_wdb2ts/test/Makefile.am \
