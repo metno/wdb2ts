c71456d89b9f531f2e64aac2904bd27ea488f198
Børge Moe <borge.moe@met.no>
2016-10-03 11:19:46 +0200

    Fix toBeufort to be in accordance with the beaufort table in m/s.

--- src/miutil/metfunctions.cpp
+++ src/miutil/metfunctions.cpp
@@ -26,7 +26,9 @@
     MA  02110-1301, USA
 */
 
+
 #include <math.h>
+#include <iostream>
 #include "metfunctions.h"
 
 namespace {
@@ -39,6 +41,7 @@ const float ewt[41] = { .000034,.000089,.000220,.000517,.001155,.002472,
                          473.67, 578.09, 701.13, 845.28, 1013.25 };
 }
 
+using namespace std;
 
 namespace miutil {
 
@@ -80,6 +83,144 @@ dewPointTemperature( float ta, float rh )
 
    return (td<=ta ? td : ta);
 }
+
+
+std::string toBeaufort( float mps, std::string &name )
+{
+	if( mps == FLT_MAX ){
+		name = "";
+		return "";
+	}
+
+	//Round to nearst 10th and convert to integer.
+	long int iMps=lround(mps*10);
+
+	if( iMps < 3 ){  //0.0-0.2 m/s
+		name = "Stille";
+		return "0";
+	}else if( iMps < 16 ){ //0.3-1.5 m/s
+		name = "Flau vind";
+		return "1";
+	}else if( iMps < 34 ){ //1.6-3.3 m/s
+		name = "Svak vind";
+		return "2";
+	}else if( iMps < 55 ){ //3.4-5.4 m/s
+		name = "Lett bris";
+		return "3";
+	}else if( iMps < 80 ){ //5.5-7.9 m/s
+		name = "Laber bris";
+		return "4";
+	}else if( iMps < 108 ){ //8.0-10.7 m/s
+		name = "Frisk bris";
+		return "5";
+	}else if( iMps < 139 ){ //10.8-13.8 m/s
+		name = "Liten kuling";
+		return "6";
+	}else if( iMps < 172 ){ //13.9-17.1 m/s
+		name = "Stiv kuling";
+		return "7";
+	}else if( iMps < 208 ){ //17.2-20.7 m/s
+		name = "Sterk kuling";
+		return "8";
+	}else if( iMps < 245 ){ //20.8-24.4 m/s
+		name = "Liten storm";
+		return "9";
+	}else if( iMps < 285 ){ //24.5-28.4 m/s
+		name = "Full storm";
+		return "10";
+	}else if( iMps < 327 ){ //28.5-32.6 m/s
+		name = "Sterk storm";
+		return "11";
+	}else{
+		name = "Orkan";
+		return "12";
+	}
+
+	return name;
+}
+
+
+std::string windDirectionName( float dd )
+{
+	if( dd == FLT_MAX )
+		return "";
+
+	if( dd < 0 || dd > 360 )
+		dd = 360;
+
+	if( dd < 22.5 || dd >= 337.5 )
+		return "N";
+	else if( dd >= 22.5 && dd < 67.5 )
+		return "NE";
+	else if( dd >= 67.5 && dd < 112.5 )
+		return "E";
+	else if( dd >= 112.5 && dd < 157.5 )
+		return "SE";
+	else if( dd >= 157.5 && dd < 202.5 )
+		return "S";
+	else if( dd >= 202.5 && dd < 247.5 )
+		return "SW";
+	else if( dd >= 247.5 && dd < 292.5 )
+		return "W";
+	else
+		return "NW";
+}
+
+std::string symbolidToName( int id )
+{
+	if( id < 0 )
+		return "";
+
+	switch( id ){
+	case 1:
+	case 16:
+		return "SUN";
+	case 2:
+	case 17:
+		return "LIGHTCLOUD";
+	case 3:
+		return "PARTLYCLOUD";
+	case 4:
+		return "CLOUD";
+	case 5:
+		return "LIGHTRAINSUN";
+	case 18:
+		return "LIGHTRAINSUN";
+	case 6:
+		return "LIGHTRAINTHUNDERSUN";
+	case 7:
+		return "SLEETSUN";
+	case 8:
+		return "SNOWSUN";
+	case 9:
+		return "LIGHTRAIN";
+	case 10:
+		return "RAIN";
+	case 11:
+		return "RAINTHUNDER";
+	case 12:
+		return "SLEET";
+	case 13:
+		return "SNOW";
+	case 14:
+		return "SNOWTHUNDER";
+	case 15:
+		return "FOG";
+	case 19:
+		return "SNOWSUN";
+	case 20:
+		return "SLEETSUNTHUNDER";
+	case 21:
+		return "SNOWSUNTHUNDER";
+	case 22:
+		return "LIGHTRAINTHUNDER";
+	case 23:
+		return "SLEETTHUNDER";
+	default:
+		return "";         //Unknown symbol
+	}
+}
+
 
 
 }
--- src/miutil/metfunctions.h
+++ src/miutil/metfunctions.h
@@ -30,6 +30,7 @@
 #define __METFUNCTIONS_H__
 
 #include <float.h>
+#include <string>
 
 namespace miutil {
 
@@ -54,6 +55,66 @@ dewPointTemperatureToRelativeHumidity( float temperature, float dewPointTemperat
  */
 float
 dewPointTemperature( float temperature, float relativeHumidity );
+
+
+/**
+ * Compute the Beaufort value and name (in norwegian).
+ *
+ * | code | Name       | m/s     |knop | Description                                                                                                                              |
+ * |:----:|:-----------|:-------:|:---:|:-----------------------------------------------------------------------------------------------------------------------------------------|
+ * |     0|Stille      |0,0-0,2  |0-1  |Røyken stiger rett opp.                                                                                                                   |
+ * |     1|Flau vind   |0,3-1,5 	|1-3 	|En kan se vindretningen av røykens drift.                                                                                                 |
+ * |     2|Svak vind   |1,6-3,3 	|4-6 	|En kan føle vinden. Bladene på trærne rører seg, vinden kan løfte små vimpler.                                                            |
+ * |     3|Lett bris   |3,4-5,4 	|7-10 |Løv og småkvister rører seg. Vinden strekker lette flagg og vimpler.                                                                      |
+ * |     4|Laber bris  |5,5-7,9 	|11-16|Vinden løfter støv og løse papirer, rører på kvister og smågreine, strekker større flagg og vimpler.                                      |
+ * |     5|Frisk bris  |8,0-10,7 |17-21|Småtrær med løv begynner å svaie. På vann begynner småbølgene å toppe seg                                                                 |
+ * |     6|Liten kuling|10,8-13,8|22-27|Store greiner og mindre stammer rører seg. Det hviner i telefonledninger. Det er vanskelig å bruke paraply. En merker motstand når en går.|
+ * |     7|Stiv kuling |13,9-17,1|28-33|Hele trær rører på seg. Det er tungt å gå mot vinden.                                                                                     |
+ *	|     8|Sterk kuling|17,2-20,7|34-40|Vinden brekker kvister av trærne. Det er tungt å gå mot vinden.                                                                           |
+ *	|     9|Liten storm |20,8-24,4|41-47|Hele store trær svaier og hiver. Takstein kan blåse ned.                                                                                  |
+ * |    10|Full storm  |24,5-28,4|48-55|Sjelden inne i landet. Trær rykkes opp med rot. Stor skade på hus.                                                                        |
+ * |    11|Sterk storm |28,5-32,6|56-63|Forekommer sjelden og følges av store ødeleggelser.                                                                                       |
+ * |    12|Orkan       |32,7- 	|64- 	|Forekommer meget sjelden. Uvanlig store ødeleggelser.                                                                                     |
+ *
+ * \param[in] mps The wind in meter per second.
+ * \param[out] name The beaufort name in norwegian.
+ * \return The Beaufort code as a string. If mps is FLT_MAX an empty string is returned and name is also set to an empty string.
+ */
+
+std::string
+toBeaufort( float mps, std::string &name );
+
+
+
+/**
+ * Compute the wind direction name.
+ * Direction is given in degrees
+ *
+ * |name|Direction  |
+ * |:--:|:----------|
+ * |N   |337.5-22.5 |
+ *	|NE  |22.5-67.5  |
+ *	|E   |67.5-112.5 |
+ *	|SE  |112.5-157.5|
+ *	|S   |157.5-202.5|
+ *	|SW  |202.5-247.5|
+ *	|W   |247.5-292.5|
+ *	|NW  |292.5-337.5|
+ *
+ *	\param dd Wind direction in degrees.
+ *	\return The name. If dd is FLT_MAX an empty string is returned.
+ */
+std::string
+windDirectionName( float dd );
+
+
+/**
+ * Return the SYMBOLNAME given an symbolid.
+ * \note this is not used anymore. Use the methods in the weather symbol library.
+ */
+std::string
+symbolidToName( int id );
+
 
 }
 
